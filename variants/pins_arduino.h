9a4cb27a502ffe7aa691d1f77aa18725553056ce
Kamillo Ferry <kafe@kafe.dev>
2023-09-13 00:19:43 +0200

    Basic working automake setup

---
+++ variants/pins_arduino.h
@@ -0,0 +1,23 @@
+#if defined(VARIANT_STANDARD)
+#include <variants/standard/pins_arduino.h>
+#elif defined(VARIANT_CIRCUITPLAY32U4)
+#include <variants/circuitplay32u4/pins_arduino.h>
+#elif defined(VARIANT_EIGHTANALOGINPUTS)
+#include <variants/eightanaloginputs/pins_arduino.h>
+#elif defined(VARIANT_ETHERNET)
+#include <variants/ethernet/pins_arduino.h>
+#elif defined(VARIANT_GEMMA)
+#include <variants/gemma/pins_arduino.h>
+#elif defined(VARIANT_LEONARDO)
+#include <variants/leonardo/pins_arduino.h>
+#elif defined(VARIANT_MEGA)
+#include <variants/mega/pins_arduino.h>
+#elif defined(VARIANT_MICRO)
+#include <variants/micro/pins_arduino.h>
+#elif defined(VARIANT_ROBOT_CONTROL)
+#include <variants/robot_control/pins_arduino.h>
+#elif defined(VARIANT_ROBOT_MOTOR)
+#include <variants/robot_motor/pins_arduino.h>
+#elif defined(VARIANT_YUN)
+#include <variants/yun/pins_arduino.h>
+#endif
